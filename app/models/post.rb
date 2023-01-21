eb1514ab01d176b9e50d9cb0f190e201994410a0
tech-zack <difference19@icloud.com>
2023-01-21 12:47:33 +0900

    投稿機能の作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :image, presence: true 
+  validates :title, presence: true, unless: :was_attached?
+  validates :text, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'libraries/index'
   get 'acounts/show'
   devise_for :users
   get 'posts/index'
-  root to: "posts#index"
+  root to: 'libraries#index'
+  resource :posts, only: [:index, :new, :create]
   resources :acounts, only: :show
 end
