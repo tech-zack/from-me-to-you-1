dd7dabe42a8129a1662d8cf67fd3d816fb32de03
tech-zack <difference19@icloud.com>
2023-01-23 19:27:08 +0900

    投稿編集機能並びに削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create]
+  before_action :authenticate_user!, only: [:new,:create,:edit,:update,:destroy]
 
   def index
     @posts = Post.all.page(params[:page]).per(3)
@@ -21,6 +21,26 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to root_path
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: 'libraries#index'
-  resources :posts, only: [:index, :new, :create, :show]
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :acounts, only: :show
   resources :libraries, only: [:index, :show]
 end
