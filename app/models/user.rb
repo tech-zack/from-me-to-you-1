5a8fe8d8e6614471faa020a8b4b3b5e925b4d777
tech-zack <difference19@icloud.com>
2023-02-01 23:13:30 +0900

    ユーザー登録機能の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname,presence: true
-  validates :image,presence: true
+  # validates :image,presence: true
   validates :profile, length: { maximum: 200 }
   has_many :posts, dependent: :destroy 
   has_many :comments, dependent: :destroy
