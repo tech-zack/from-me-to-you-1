bf40abdb529936781555e51e220d01793286da21
tech-zack <difference19@icloud.com>
2023-02-12 07:57:25 +0900

    Hotwire導入並びにデザイン変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '言葉' },
+    { id: 2, name: '思いで' },
     { id: 3, name: '風景' },
     { id: 4, name: '宝物' },
   ]
