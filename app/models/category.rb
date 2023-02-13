c9f307b7c229a0775b6ab0700cadedf592736c20
tech-zack <difference19@icloud.com>
2023-02-13 23:06:57 +0900

    個人ページのデザイン変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '思いで' },
+    { id: 2, name: '思い出' },
     { id: 3, name: '風景' },
     { id: 4, name: '宝物' },
     { id: 5, name: '言葉' },
