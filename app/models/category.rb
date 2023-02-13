241d7d1802b215327fda44c92fce26d1254c9b85
tech-zack <difference19@icloud.com>
2023-02-13 22:32:32 +0900

    カテゴリー追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,8 @@ class Category < ActiveHash::Base
     { id: 2, name: '思いで' },
     { id: 3, name: '風景' },
     { id: 4, name: '宝物' },
+    { id: 5, name: '言葉' },
+    { id: 6, name: '知恵袋' },
   ]
 
   include ActiveHash::Associations
