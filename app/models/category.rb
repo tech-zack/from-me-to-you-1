4c728ea095fd366a48651094dac542b6a4b29208
tech-zack <difference19@icloud.com>
2023-02-15 10:40:56 +0900

    カテゴリーの内容変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,7 +5,7 @@ class Category < ActiveHash::Base
     { id: 3, name: '風景' },
     { id: 4, name: '宝物' },
     { id: 5, name: '言葉' },
-    { id: 6, name: '知恵袋' },
+    { id: 6, name: '雑学' },
   ]
 
   include ActiveHash::Associations
